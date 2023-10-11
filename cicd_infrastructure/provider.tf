6013860a3c065b77eeda5c5316546a0f2498a77c
Maikel Penz <maikel.penz@plexure.com>
2023-10-11 19:54:47 +1300

    upgrading versions

--- cicd_infrastructure/provider.tf
+++ cicd_infrastructure/provider.tf
@@ -4,6 +4,13 @@ provider "aws" {
 }
 
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
   backend "s3" {
     key = "job-scraper/terraform.tfstate"
   }
